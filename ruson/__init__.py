ae98ce70b50187a056324535e9a50465624e6ce9
João Vitor Bernardi Severo <joaovbsevero@gmail.com>
2023-11-20 12:12:27 -0300

    Add import shortcut

--- ruson/__init__.py
+++ ruson/__init__.py
@@ -1,3 +1,6 @@
+from .core.config import Config
+from .core.instance import Ruson
+from .core.ruson_doc import RusonDoc
 from .driver.client import create_client
 from .driver.results import *
 from .driver.types import *
